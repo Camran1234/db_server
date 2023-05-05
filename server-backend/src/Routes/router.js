92081fe3afd548accceffd21ee402faeb1d148b1
Camran1234 <Camran750@gmail.com>
2023-05-05 03:40:17 -0600

    [FIx]

--- server-backend/src/Routes/router.js
+++ server-backend/src/Routes/router.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const handlerController = require('../Controller/handlerController')
 
-router.get('/getAdmins', handlerController.getAdmins)
+router.post('/getAdmins', handlerController.getAdmins)
+router.post('/getDesarrolladores', handlerController.getDesarrolladores)
+router.post('/getMonedas', handlerController.getMonedas)
+router.post('/getDept', handlerController.getDept)
+router.post('/getNoticias', handlerController.getNoticias)
 
 module.exports = router;
