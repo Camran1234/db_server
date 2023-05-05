c0588dba248aa74875e908439dd02bb65f3dd03c
Camran1234 <Camran750@gmail.com>
2023-05-05 02:40:03 -0600

    [Add]

--- server-backend/src/Config/db.js
+++ server-backend/src/Config/db.js
@@ -1,10 +1,13 @@
 const mysql = require('mysql2')
 
 const configure = {
-    host: 'db-network.c7a1pogzvzij.us-east-1.rds.amazonaws.com',
+    //host: 'db-network.c7a1pogzvzij.us-east-1.rds.amazonaws.com',
+    host: '54.226.142.44',
     user: 'admin',
     multipleStatements: true,
     password: 'DemonHunter1234.-',
+    database: 'db_redes',
+    port: '3306'
     
 }
 
