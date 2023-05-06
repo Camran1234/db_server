778aa3233984c754cf1a5c7d08b1a24859694974
Camran1234 <Camran750@gmail.com>
2023-05-05 23:18:29 -0600

    [Fix]

--- server-backend/src/index.js
+++ server-backend/src/index.js
@@ -10,7 +10,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb' }));
-app.use(cors());
+app.use(cors({
+    origin: 'http://3.91.76.153'
+}));
 
 const server = app.listen(app.get('port'), () => {
     console.log('Server On Port ', app.get('port'))
